6e572c0e35da8162f8ef80bac800476b4ef7dad6
N3crates <gtoz6164@gmail.com>
2026-05-12 12:29:43 -0600

    Se arreglo la conexion del backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.status(200).json({
-    success: true,
+app.get("/",(req, res) => {
+    res.json({
     message: 'API running correctly'
   });
 });
