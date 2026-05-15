c2da1bb3e893afab511fb2f5f8cdd96b1e54771c
juan7679 <juanxx514@gmail.com>
2026-05-14 20:09:24 -0600

    se registraron rutas de teachers, subjects y groups en app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,17 @@ const app = express();
 const authRoutes = require('./modules/auth/auth.routes');
 const studentRoutes = require("./modules/students/student.routes")
 const enrollmentRoutes = require("./modules/enrollments/enrollment.routes")
-const gradeRoutes = require("./modules/grades/grade.routes")
+const teacherRoutes = require("./modules/teachers/teachers.routes")
+const subjectRoutes = require("./modules/subjects/subjects.routes")
+const groupRoutes = require("./modules/groups/groups.routes")
 
 app.use(cors())
 app.use(express.json());
 app.use("/api/students", studentRoutes)
 app.use("/api/enrollments", enrollmentRoutes)
-app.use("/api/grades", gradeRoutes)
+app.use("/api/teachers", teacherRoutes)
+app.use("/api/subjects", subjectRoutes)
+app.use("/api/groups", groupRoutes)
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({
