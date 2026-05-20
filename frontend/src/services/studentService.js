66c39c26806375bd10187c897d64adee3ac9df12
N3crates <gtoz6164@gmail.com>
2026-05-20 15:42:23 -0600

    Se agrego actualizar y dar de baja students, ademas se mejoro la interfaz grafica

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -11,3 +11,31 @@ export const getStudents = async () => {
         throw error;
     }
 };
+
+export const createStudent = async (studentData) => {
+    try {
+        const response = await axios.post(API_URL, studentData)
+        return response.data
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+export const updateStudent = async(id, studentData) => {
+    try {
+        const response = await axios.put(`${API_URL}/${id}`, studentData)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const deleteStudent = async(id) => {
+    try {
+        const response = await axios.delete(`${API_URL}/${id}`)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
