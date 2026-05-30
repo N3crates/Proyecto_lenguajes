6c95bd3d57f000517a0381f981f1ce924113e682
N3crates <gtoz6164@gmail.com>
2026-05-30 12:28:07 -0600

    Actualizacion modulo calificaciones y bajas en cascada

--- backend/src/modules/grades/grade.repository.js
+++ backend/src/modules/grades/grade.repository.js
@@ -2,7 +2,7 @@ const db = require("../../config/firebase")
 const collection = db.collection("grades")
 
 const getAllGrades = async() => {
-    const snapshot = await collection.where("status", "==", true).get()
+    const snapshot = await collection.get()
 
     return snapshot.docs.map(doc => ({
         id: doc.id,
@@ -16,16 +16,25 @@ const createGrade = async(gradeData) => {
     return { id: response.id }
 }
 
-const getGradesByEnrollmentId = async(enrollmentId) => {
-    const snapshot = await collection
-        .where("enrollmentId", "==", enrollmentId)
-        .where("status", "==", true)
-        .get()
-    
-    return snapshot.docs.map(docs => ({
+const getGradesByEnrollmentId = async (enrollmentId) => {
+    const snapshot = await collection.where("enrollmentId", "==", enrollmentId).where("status", "==", true).get()
+    return snapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
     }))
+}
+
+const findByEnrollmentId = async (enrollmentId) => {
+    const snapshot = await collection.where("enrollmentId", "==", enrollmentId).where("status", "==", true).get()
+    if(snapshot.empty){
+        return null
+    }
+
+    const doc = snapshot.docs[0]
+    return {
+        id: doc.id,
+        ...doc.data()
+    }
 }
 
 const updateGrade = async(id, gradeData) => {
@@ -41,10 +50,30 @@ const deleteGrade = async(id) => {
     return{ id }
 }
 
+const deleteGradesByEnrollmentId = async (enrollmentId) => {
+
+    const snapshot = await collection
+        .where("enrollmentId", "==", enrollmentId)
+        .where("status", "==", true)
+        .get();
+
+    await Promise.all(
+        snapshot.docs.map(doc =>
+            doc.ref.update({
+                status: false,
+                deletedAt: new Date()
+            })
+        )
+    );
+
+};
+
 module.exports = {
     getAllGrades,
     createGrade,
     getGradesByEnrollmentId,
+    findByEnrollmentId,
     updateGrade,
-    deleteGrade
+    deleteGrade,
+    deleteGradesByEnrollmentId
 }
--- backend/src/modules/grades/grade.validation.js
+++ backend/src/modules/grades/grade.validation.js
@@ -13,7 +13,24 @@ const validateGrade = (gradeData) => {
     if(gradeData.partial3 == null){
         errors.push("Parcial 3 obligatorio")
     }
-
+    if (gradeData.partial1 < 0 || gradeData.partial1 > 10
+    ) {
+    errors.push(
+        "Parcial 1 debe estar entre 0 y 10"
+    )
+    }
+    if (gradeData.partial2 < 0 || gradeData.partial2 > 10
+    ) {
+    errors.push(
+        "Parcial 2 debe estar entre 0 y 10"
+    )
+    }
+    if (gradeData.partial3 < 0 || gradeData.partial3 > 10
+    ) {
+    errors.push(
+        "Parcial 3 debe estar entre 0 y 10"
+    )
+    }
     return errors
 }
 
